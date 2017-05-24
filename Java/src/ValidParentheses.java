e7c270c09ae6519892a27008a310103259b54d12
jcao82 <jackysfbay@gmail.com>
2017-05-24 17:51:03 +0800

    Added ValidParentheses

---
+++ Java/src/ValidParentheses.java
@@ -0,0 +1,15 @@
+public class ValidParentheses {
+	/**
+	 * Given a string containing just the characters '(', ')', '{', '}', '[' and
+	 * ']', determine if the input string is valid.
+	 * 
+	 * The brackets must close in the correct order, "()" and "()[]{}" are all
+	 * valid but "(]" and "([)]" are not. Õ»·¨
+	 */
+
+	public static boolean solution(String str) {
+
+		
+		return false;
+	}
+}
